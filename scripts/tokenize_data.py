232ea7d75f0efa6ecd97c7746c7c900131f906c8
isaiah1810 <isaiah1810@gmail.com>
2025-03-25 16:35:28 -0400

    Updated code

--- scripts/tokenize_data.py
+++ scripts/tokenize_data.py
@@ -24,71 +24,163 @@ def preprocess_frame(path, size):
     img = img * 2 - 1
     return img.unsqueeze(0)
 
-def tokenize_video(input_path, size, model, device, sample_len=13):
-    """Tokenize all frames in a video directory."""
+# def tokenize_video(input_path, size, model, device, ep_len=100, sample_len=20):
+#     """Tokenize all frames in a video directory."""
+#     import torch
+#     import os
+#     from torchvision.transforms import ToTensor
+#     from PIL import Image
+    
+#     frames = []
+#     files = sorted([os.path.join(input_path, file) for file in os.listdir(input_path) 
+#                    if file.lower().endswith(('.png', '.jpg', '.jpeg'))])
+#     print(input_path, os.listdir(input_path))
+#     all_tokens = []
+        
+#     for file_path in files:
+#         img = Image.open(file_path)
+#         img = img.resize((size, size))
+#         img = ToTensor()(img)
+#         frames.append(img)
+
+#     ep_len = len(frames)
+#     seq_per_episode = ep_len - sample_len + 1
+    
+#     for i in range(seq_per_episode):
+#         seq_frames = frames[i:i+sample_len]
+
+#         if seq_frames:
+#                 remainder = (len(seq_frames) - 1) % 4
+#                 if remainder != 0:
+#                     seq_frames.extend([seq_frames[-1]] * (4 - remainder))
+#                 frame_tensor = torch.stack(seq_frames, dim=2).reshape((1, 3, -1, size, size)).to(device)
+#                 tokens = model.encode(frame_tensor, False)
+#                 all_tokens.append(tokens)
+    
+#     if all_tokens:
+#         return all_tokens
+#     return torch.tensor([])
+
+def tokenize_video(input_path, size, model, device, ep_len=100, sample_len=20):
     import torch
     import os
     from torchvision.transforms import ToTensor
     from PIL import Image
-    
-    frames = []
-    files = sorted([os.path.join(input_path, file) for file in os.listdir(input_path) 
+
+
+    paths = sorted([os.path.join(input_path, file) for file in os.listdir(input_path) 
                    if file.lower().endswith(('.png', '.jpg', '.jpeg'))])
-    print(input_path, os.listdir(input_path))
-    all_tokens = []
-        
-    for file_path in files:
+
+    seqs_per_episode = ep_len - sample_len + 1
+
+    paths = os.listdir(input_path)
+
+    # Pad To Meet Episode Length
+    while len(paths) < ep_len:
+            paths.append(paths[-1])
+
+    frames = []
+    for file_path in paths:
         img = Image.open(file_path)
         img = img.resize((size, size))
         img = ToTensor()(img)
         frames.append(img)
 
-    ep_len = len(frames)
-    seq_per_episode = ep_len - sample_len + 1
-    
-    for i in range(seq_per_episode):
-        seq_frames = frames[i:i+sample_len]
 
+    seq_tokens = []
+    
+    for i in range(seqs_per_episode):
+        seq_frames = torch.stack(frames[i:i+sample_len], dim=0).to(device)
 
         if seq_frames:
-                remainder = (len(seq_frames) - 1) % 4
-                if remainder != 0:
-                    seq_frames.extend([seq_frames[-1]] * (4 - remainder))
-                frame_tensor = torch.stack(seq_frames, dim=2).reshape((1, 3, -1, size, size)).to(device)
-                tokens = model.encode(frame_tensor, False)
-                all_tokens.append(tokens)
-    
-    if all_tokens:
-        return all_tokens
-    return torch.tensor([])
+            tokens = model.encode(seq_frames, True)
+            seq_tokens.append(tokens)
 
-def vqgan_tokenizer(input_path: str, output_path: str, size: int, device: str):
+    return seq_tokens
+
+
+def vqgan_tokenizer(input_path: str, output_path: str, size: int, device: str, ep_len: int, sample_len: int):
     """Tokenize videos using VQGAN."""
     import os
     import h5py
     import torch
     from tqdm import tqdm
     
+    data_dir = os.path.join(input_path, 'frames')
+
     os.makedirs(output_path, exist_ok=True)
     model = load_vqgan_model().to(device)
     model.eval() 
-    
-    if all(os.path.isdir(os.path.join(input_path, d)) for d in os.listdir(input_path) if not d.startswith('.')):
-        dirs = [os.path.join(input_path, d) for d in os.listdir(input_path) if os.path.isdir(os.path.join(input_path, d))]
-    else:
-        dirs = [input_path]
 
-    with torch.no_grad():  
-        for dir_path in tqdm(dirs, desc="Processing directories"):
-            dir_name = os.path.basename(dir_path)
-            output_file = os.path.join(output_path, f"{dir_name}.h5")            
-            tokens = tokenize_video(dir_path, size, model, device)
-            print(tokens.shape)
-            if len(tokens) > 0:
-                with h5py.File(output_file, "w") as f:
-                    f.create_dataset('tokens', data=tokens.cpu().numpy(), 
-                                    compression="gzip", compression_opts=9)
-                print(f"Processed {dir_name}: {tokens.shape}")
+    # Get Training Split Episodes
+    train_json = os.path.join(input_path, 'labels/train.json')
+    train_dirs = []
+    with open(train_json, 'r') as f:
+                    json_file = json.load(f)
+                    for item in json_file:
+                        if os.path.exists(os.path.join(data_dir, item['id'])):
+                            train_dirs.append(os.path.join(data_dir, item['id']))
+
+    # Get Validation Split Episodes
+    val_json = os.path.join(input_path, 'labels/validation.json')
+    val_dirs = []
+    with open(val_json, 'r') as f:
+                    json_file = json.load(f)
+                    for item in json_file:
+                        if os.path.exists(os.path.join(data_dir, item['id'])):
+                            val_dirs.append(os.path.join(data_dir, item['id']))
+
+
+    # Get Test Split Episodes
+    test_json = os.path.join(input_path, 'labels/test.json')
+    test_dirs = []
+    with open(test_json, 'r') as f:
+                    json_file = json.load(f)
+                    for item in json_file:
+                        if os.path.exists(os.path.join(data_dir, item['id'])):
+                            test_dirs.append(os.path.join(data_dir, item['id']))
+
+
+
+    # if all(os.path.isdir(os.path.join(input_path, d)) for d in os.listdir(input_path) if not d.startswith('.')):
+    #     dirs = [os.path.join(input_path, d) for d in os.listdir(input_path) if os.path.isdir(os.path.join(input_path, d))]
+    # else:
+    #     dirs = [input_path]
+
+    train_dirs = [os.path.join(input_path, d) for d in train_dirs if os.path.isdir(os.path.join(input_path, d))]
+    val_dirs = [os.path.join(input_path, d) for d in val_dirs if os.path.isdir(os.path.join(input_path, d))]
+    test_dirs = [os.path.join(input_path, d) for d in test_dirs if os.path.isdir(os.path.join(input_path, d))]
+
+
+    with torch.no_grad():
+        seq = 0
+        for dir_path in tqdm(train_dirs, desc="Tokenizing Training Data"):
+            tokens = tokenize_video(dir_path, size, model, device, seq, ep_len, sample_len)    
+            for i in range(len(tokens)):
+                with h5py.File(os.path.join(output_path, 'train', f'{seq}.h5')):
+                      seq += 1
+                      f.create_dataset('tokens', data=tokens[i].cpu().numpy(), 
+                                       compression='gzip', compression_opts=9)
+
+        seq = 0
+        for dir_path in tqdm(val_dirs, desc="Tokenizing Validation Data"):
+            tokens = tokenize_video(dir_path, size, model, device, ep_len, sample_len)    
+            for i in range(len(tokens)):
+                with h5py.File(os.path.join(output_path, 'validation', f'{seq}.h5')):
+                      seq += 1
+                      f.create_dataset('tokens', data=tokens[i].cpu().numpy(), 
+                                       compression='gzip', compression_opts=9)
+
+        seq = 0
+        for dir_path in tqdm(val_dirs, desc="Tokenizing Validation Data"):
+            tokens = tokenize_video(dir_path, size, model, device, ep_len, sample_len)    
+            for i in range(len(tokens)):
+                with h5py.File(os.path.join(output_path, 'validation', f'{seq}.h5')):
+                      seq += 1
+                      f.create_dataset('tokens', data=tokens[i].cpu().numpy(), 
+                                       compression='gzip', compression_opts=9)
+
+
 
 if __name__ == "__main__":
     import argparse
